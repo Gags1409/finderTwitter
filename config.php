cef9d2782b3239438d5c6f0c2a799094bc21517d
Gags1409 <gagan.cheema@gmail.com>
2014-05-27 02:56:34 +1000

    Create config.php
    Config settings

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Config file
+ * oAuth Configuration.
+ */
+define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
+define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+define('ACCESS_TOKEN', 'ACCESS_TOKEN_HERE');
+define('ACCESS_SECRET', 'ACCESS_SECRET_HERE');
