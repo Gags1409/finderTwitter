aa57e23b1150ce4ffc3e48a253a7b90675562f29
Gags1409 <gagan.cheema@gmail.com>
2014-05-27 03:00:44 +1000

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,9 @@
  */
 define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
 define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+/*******************************************************
+ * Please note that oauth access token and secret key can be fetched by session by 
+ * giving callback function insted of providing the sensitive info here
+ * *****************************************************/
 define('ACCESS_TOKEN', 'ACCESS_TOKEN_HERE');
 define('ACCESS_SECRET', 'ACCESS_SECRET_HERE');
